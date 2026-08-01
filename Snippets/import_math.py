e9e159ebc8e0e8216587bd5adf315027328abc5d
DJ Idorot <dj.idorot@coalitiontechnologies.com>
2026-08-01 23:28:07 +0800

    Adding import math

---
+++ Snippets/import_math.py
@@ -0,0 +1,15 @@
+import math
+from math import pi, sqrt
+import math as m
+
+
+# Standard Import: Requires prefixing with math.
+import math
+print(math.sqrt(25))  # Outputs: 5.0
+
+# Specific Import: Import specific tools to use them directly.
+print(sqrt(16))      # Outputs: 4.0
+print(pi)            # Outputs: 3.141592653589793
+
+# Alias Import: Shorten the module name for faster typing.
+print(m.floor(4.7))   # Outputs: 4
