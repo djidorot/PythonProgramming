a9b4edcbcd6d64501318eb56b67626fbc392827c
DJ Idorot <dj.idorot@coalitiontechnologies.com>
2026-08-01 22:29:15 +0800

    Adding float()

---
+++ float.py
@@ -0,0 +1,26 @@
+# Basic Syntax
+
+# 1. Converting Integers
+print()
+print(float(7))     # Output: 7.0
+print(float(-15))   # Output: -15.0
+
+# 2. Converting Strings
+print()
+print(float("3.14"))   # Output: 3.14
+print(float("100"))    # Output: 100.0
+print(float("3e2"))    # Output: 300.0 (3 * 10^2)
+
+# 3. Converting Booleans
+print()
+print(float(True))    # Output: 1.0
+print(float(False))   # Output: 0.0
+
+# 4. Special String Values
+print()
+print(float("inf"))       # Output: inf
+print(float("-infinity")) # Output: -inf
+print(float("nan"))       # Output: nan
+print()
+
+
